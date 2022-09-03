b538306053abc6b9d8487c407f56533b6f813f46
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2022-09-03 10:29:27 +0200

    Literal octal notation

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,18 +17,41 @@ $config = new PhpCsFixer\Config();
 $config
     ->setRiskyAllowed(true)
     ->setRules([
+        'header_comment' => [
+            'header' => $header,
+            'comment_type' => 'PHPDoc',
+            'location' => 'after_open',
+            'separate' => 'bottom'
+        ],
         '@PHP74Migration' => true,
         '@PHP74Migration:risky' => true,
-        '@PHPUnit75Migration:risky' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
         'concat_space' => [
             'spacing' => 'one',
         ],
         'array_indentation' => true,
-        'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']],
-        'header_comment' => ['header' => $header, 'comment_type' => 'PHPDoc', 'location' => 'after_open', 'separate' => 'bottom'],
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => [
+                'expectedDeprecation'
+            ]
+        ],
         'use_arrow_functions' => true,
+        'control_structure_braces' => true,
+        'control_structure_continuation_position' => true,
+        'self_static_accessor' => true,
+        'ordered_interfaces' => true,
+        'phpdoc_var_annotation_correct_order' => true,
+        'return_assignment' => true,
+        'no_useless_else' => true,
+        'no_superfluous_elseif' => true,
+        'no_useless_return' => true,
+        'multiline_comment_opening_closing' => true,
+        'no_null_property_initialization' => true,
+        'operator_linebreak' => true,
+        'method_chaining_indentation' => true,
+        'strict_param' => true,
+        'strict_comparison' => true,
     ])
     ->setFinder($finder)
     ->setLineEnding(PHP_EOL);
